93b19cb4a847322fa5994268ad220db8bdc66cba
silviaesparza <silvia.esparza.oviedo@gmail.com>
2021-03-26 21:57:59 -0500

    Clase_26-03-2021

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+// Variables y constantes
+
+let nombre;
+let apellido;
+const edad = 28;
+nombre = "Silvia";
+apellido = "Esparza Oviedo";
+console.log(nombre, apellido, edad);
+
+
+// Tipos de datos
+let Nombre = `Silvia Esparza Oviedo`; //String
+let Edad = 28; //number - intenger - int;
+let altura = 1.70; //Double - float
+let profesor = true; // Boleano
+let informacion = [Nombre, Edad, altura, profesor]; //Array
+console.log(informacion);
+// console.log(informacion[2]);
+let baso = {
+	material : "porcelan",
+	diametro :  60,
+	lleno : false
+}; // Object
+console.log(baso);
+// console.log(baso.diametro)
+
+
+
+
+
